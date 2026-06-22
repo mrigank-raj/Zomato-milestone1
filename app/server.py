a37ba621010aab7aa245cd5bed6a4953cdf1d863
Mrigank Raj Chouhan <mrigankrajchouhan@gmail.com>
2026-06-23 00:13:13 +0530

    Robust CORS handling for wildcard origins

--- app/server.py
+++ app/server.py
@@ -94,10 +94,14 @@ if allowed_origins_env:
 else:
     origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
 
+allow_credentials = True
+if "*" in origins:
+    allow_credentials = False
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=allow_credentials,
     allow_methods=["*"],
     allow_headers=["*"],
 )
